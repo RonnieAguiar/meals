8ae7d36029b99b64d563d4ebdf6d6300b63df465
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-03-24 15:54:56 -0300

    Update categories_meals_screen.dart

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  final Category category;
-
+  
   const CategoriesMealsScreen({
-    Key? key,
-    required this.category,
-  }) : super(key: key);
+    Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)?.settings.arguments as Category;
+    
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
