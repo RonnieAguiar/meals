ccd106efd24bada8b19030f5c138a1746b3a722a
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-03-29 08:08:28 -0300

    Update routes
    Colocando as rotas no mesmo arquivo.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/utils/app_routes.dart';
-import 'screens/categories_screen.dart';
-import 'screens/meal_detail_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -26,11 +23,8 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      routes: {
-        AppRoutes.home: (context) => const CategoriesScreen(),
-        AppRoutes.categoryMeals: (context) => const CategoriesMealsScreen(),
-        AppRoutes.mealDetail: (context) => const MealDetailScreen(),
-      },
+      routes: myRoutes,
+      onUnknownRoute: routeUnknown,
     );
   }
 }
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,5 +1,23 @@
-class AppRoutes{
+import 'package:flutter/material.dart';
+
+import '../screens/categories_meals_screen.dart';
+import '../screens/categories_screen.dart';
+import '../screens/meal_detail_screen.dart';
+
+class AppRoutes {
   static const home = '/';
   static const categoryMeals = '/categories-meals';
   static const mealDetail = '/meal-detail';
+}
+
+var myRoutes = {
+  AppRoutes.home: (context) => const CategoriesScreen(),
+  AppRoutes.categoryMeals: (context) => const CategoriesMealsScreen(),
+  AppRoutes.mealDetail: (context) => const MealDetailScreen(),
+};
+
+MaterialPageRoute routeUnknown(settings) {
+  return MaterialPageRoute(
+    builder: (_) => const CategoriesScreen(),
+  );
 }
