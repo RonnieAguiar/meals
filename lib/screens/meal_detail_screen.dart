1cfddb7ccfcb407305f0218dbb27eff038ed3070
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-04-04 08:06:23 -0300

    Update meal_detail_screen.dart

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -89,6 +89,12 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).pop();
+          },
+        child: const Icon(Icons.star),
+      ),
     );
   }
 }
