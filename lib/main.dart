716c7165acfeac6aa7b1ff01ab10764cea5b5f65
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-04-08 21:21:40 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:meals/utils/app_routes.dart';
+
+import '../data/dammy_data.dart';
+import '../models/meal.dart';
+import '../screens/categories_meals_screen.dart';
+import '../screens/categories_screen.dart';
+import '../screens/tabs_screen.dart';
+import '../screens/meal_detail_screen.dart';
+import '../screens/settings_screen.dart';
+import 'models/settings.dart';
+import 'utils/app_routes.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+List<Meal> _availableMeals = dummyMeal;
+
+MaterialPageRoute routeUnknown(settings) {
+  return MaterialPageRoute(
+    builder: (_) => const CategoriesScreen(),
+  );
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void _filterMeals(Settings settings) {
+    setState(() {
+      _availableMeals = dummyMeal.where((meal) {
+        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+        return !filterGluten &&
+            !filterLactose &&
+            filterVegan &&
+            filterVegetarian;
+      }).toList();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+    var myRoutes = {
+      AppRoutes.home: (context) => const TabsScreen(),
+      AppRoutes.categoryMeals: (context) =>
+          CategoriesMealsScreen(_availableMeals),
+      AppRoutes.mealDetail: (context) => const MealDetailScreen(),
+      AppRoutes.settings: (context) => SettingsScreen(_filterMeals),
+    };
+
     return MaterialApp(
       title: 'Vamos cozinhar?',
       theme: ThemeData(
