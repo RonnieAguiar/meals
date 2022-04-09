2e6744fc3feff0301f3bbe37290a4335e70e5513
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-04-08 21:21:22 -0300

    Update app_routes.dart

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,28 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:meals/screens/tabs_screen.dart';
-
-import '../screens/categories_meals_screen.dart';
-import '../screens/categories_screen.dart';
-import '../screens/tabs_screen.dart';
-import '../screens/meal_detail_screen.dart';
-import '../screens/settings_screen.dart';
-
 class AppRoutes {
   static const home = '/';
   static const categoryMeals = '/categories-meals';
   static const mealDetail = '/meal-detail';
   static const settings = '/settings';
-}
-
-var myRoutes = {
-  AppRoutes.home: (context) => const TabsScreen(),
-  AppRoutes.categoryMeals: (context) => const CategoriesMealsScreen(),
-  AppRoutes.mealDetail: (context) => const MealDetailScreen(),
-  AppRoutes.settings: (context) => const SettingsScreen(),
-};
-
-MaterialPageRoute routeUnknown(settings) {
-  return MaterialPageRoute(
-    builder: (_) => const CategoriesScreen(),
-  );
 }
